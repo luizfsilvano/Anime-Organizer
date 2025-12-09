d95734d8df280c2a007a66d24faddc6b71f9c7d0
luizfsilvano <luizfsilvano@gmail.com>
2025-12-09 01:33:47 -0300

    feat(auth): cria entidade Usuario e repository

---
+++ src/main/java/com/luiz/animeorganizer/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.luiz.animeorganizer.repository;
+
+import com.luiz.animeorganizer.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email); // SELECT * FROM usuario WHERE email = ?
+
+}
